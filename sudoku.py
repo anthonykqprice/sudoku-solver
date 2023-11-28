0ac710d87941384405a82bdc7715e62fdb5f0567
Anthony Price <anthonykqprice@gmail.com>
2023-11-28 17:04:56 -0500

    Fix solver to solve all puzzles.
    Fixed an issue where the solver wouldn't reset a tile to 0 after reaching 9.

--- sudoku.py
+++ sudoku.py
@@ -1,13 +1,13 @@
 sudoku = [
-    [9,0,8,0,0,4,1,6,2],
-    [3,1,2,6,8,5,4,7,9],
-    [6,7,4,2,1,9,5,3,8],
-    [7,8,1,9,2,6,3,4,5],
-    [4,3,9,8,5,1,7,2,6],
-    [0,2,0,0,7,3,8,9,0],
-    [8,6,3,0,9,7,0,5,0],
-    [0,9,5,0,4,2,0,0,0],
-    [0,0,0,0,0,0,9,0,0]
+    [0,0,8,0,0,0,4,0,0],
+    [0,5,0,3,0,0,7,0,1],
+    [3,0,0,0,0,2,0,0,0],
+    [0,0,0,1,0,0,0,4,0],
+    [0,6,0,0,0,0,0,2,0],
+    [0,0,7,0,0,9,1,0,6],
+    [0,0,9,0,8,0,0,0,0],
+    [0,7,0,2,0,0,3,0,5],
+    [0,0,0,0,0,0,0,6,0]
 ]
 
 def solve_sudoku(sudoku):
@@ -34,11 +34,13 @@ def solve_sudoku(sudoku):
             else:
                 return next_sudoku
         else:
+            sudoku[row_0][column_0] = 0
             return "NO SOLUTION"
             
 def is_valid(sudoku):
     for row in sudoku:
         if is_valid_group(row) == False:
+            #print(f'row invalid at {row}')
             return False
     
     for column in range(0,9):
@@ -46,6 +48,7 @@ def is_valid(sudoku):
         for row in range(0,9):
             column_list.append(sudoku[row][column])
         if is_valid_group(column_list) == False:
+            #print(f'column invalid at {column}')
             return False
     
     for upper_corner_row in range(0, 9, 3):
@@ -55,6 +58,7 @@ def is_valid(sudoku):
                 for j in range(0,3):
                     grid_list.append(sudoku[upper_corner_row + i][upper_corner_column + j])
             if is_valid_group(grid_list) == False:
+                #print(f'grid invalid at {row} {column}')
                 return False
 
     return True
@@ -76,4 +80,13 @@ def is_complete(sudoku):
 
     return True
 
-print(solve_sudoku(sudoku))
+def print_sudoku(sudoku):
+    if sudoku == 'NO SOLUTION':
+        print(sudoku)
+    else:
+        for row in sudoku:
+            print(row)
+
+print_sudoku(solve_sudoku(sudoku))
+
+
